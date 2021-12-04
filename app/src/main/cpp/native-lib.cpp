8a777960b5b7fff5d17e54fb2edf89f79609b210
LeiZhi.Mai <maiyethom4@hotmail.com>
2021-12-04 20:24:33 +0800

    删除app，加入usbCameraTest

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_serenegiant_app_MainActivity_stringFromJNI(JNIEnv* env, jobject thiz) {
-	std::string hello = "Hello from C++";
-	return env->NewStringUTF(hello.c_str());
-}
